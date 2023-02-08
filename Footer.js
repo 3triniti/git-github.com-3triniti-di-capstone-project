c09b13603ba1cf90b1aea543f42d7d00e7898613
3triniti <102638725+3triniti@users.noreply.github.com>
2023-02-08 08:16:07 -1000

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,189 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo2 from "../Images/Kauai Tea Ceremony logo (12).png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import { Container } from "react-bootstrap";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+function Footer() {
+  return (
+    <>
+      <div className="flex mt-8 md:flex bg-slate-100 w-screen lg:h-64 lg:mt-2">
+        <Link to="/" className=" hover:shadow-slate-200">
+          <img
+            src={logo2}
+            className="h-60 w-60 md:ml-0 md:w-64 md:h-64 drop-shadow-lg hover:opacity-50"
+            alt=""
+          />
+        </Link>
+        <div className="ml-5 columns-3  lg:ml-10 flex-wrap md:columns-2 md:pl-24 w-1/3 mt-4 mb-3">
+          <div className="pb-2.5">
+            <Link
+              to="/"
+              className="text-teal-600 hover:text-white font-open text-xs"
+            >
+              HOME
+            </Link>
+          </div>
+          <div>
+            <div className="pb-2.5">
+              <Link
+                to="/special-events"
+                className="text-teal-600 hover:text-white font-open text-xs"
+              >
+                SPECIAL EVENTS
+              </Link>
+            </div>
+            <div className="pb-2.5">
+              <Link
+                to="/weddings"
+                className=" text-teal-600 hover:text-white font-open text-xs"
+              >
+                WEDDINGS
+              </Link>
+            </div>
+            <div className="pb-2.5">
+              <Link
+                to="/retreats"
+                className="text-teal-600 hover:text-white font-open text-xs"
+              >
+                RETREATS
+              </Link>
+            </div>
+            <div className="pb-2.5">
+              <Link
+                to="/community-outreach"
+                className="text-teal-600 hover:text-white font-open text-xs"
+              >
+                COMMUNITY OUTREACH
+              </Link>
+            </div>
+            <div className="pb-2.5">
+              <Link
+                to="/workshops"
+                className=" text-teal-600 hover:text-white font-open text-xs"
+              >
+                WORKSHOPS
+              </Link>
+            </div>
+            <div className="pb-2.5">
+              <Link
+                to="/yukata-rental"
+                className="text-teal-600 hover:text-white font-open text-xs"
+              >
+                YUKATA RENTAL
+              </Link>
+            </div>
+            <div className="pb-2.5">
+              <Link
+                to="/cups-and-plates"
+                className="text-teal-600 hover:text-white font-open text-xs"
+              >
+                CUPS AND PLATES
+              </Link>
+            </div>
+            <div className="pb-2.5">
+              <Link
+                to="/pots-and-bowls"
+                className="text-teal-600 hover:text-white font-open text-xs"
+              >
+                POTS AND BOWLS
+              </Link>
+            </div>
+            <div className="pb-2.5">
+              <Link
+                to="/tea"
+                className="text-teal-600 hover:text-white font-open text-xs"
+              >
+                TEA
+              </Link>
+            </div>
+            <div className="pb-2.5">
+              <Link
+                to="/place-mats"
+                className="text-teal-600 hover:text-white font-open text-xs"
+              >
+                PLACE MATS
+              </Link>
+            </div>
+            <div className="pb-2.5">
+              <Link
+                to="/history"
+                className="text-teal-600 hover:text-white font-open text-xs"
+              >
+                HISTORY
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="ml-0 h-48 w-0.5 mt-8 md:ml-12 bg-white"></div>
+        <div className="ml-7 md:ml-24">
+          <Container className="w-10 h-10 mt-10">
+            <div>
+              <a href="http://facebook.com">
+                <FontAwesomeIcon
+                  icon={faFacebook}
+                  className="w-7 h-7 text-teal-500 hover:text-slate-300"
+                />
+              </a>
+            </div>
+            <div>
+              <a href="http://instagram.com">
+                <FontAwesomeIcon
+                  icon={faInstagram}
+                  className="w-7 h-7 mt-3 text-teal-500 hover:text-slate-300"
+                />
+              </a>
+            </div>
+            <div>
+              <a href="http://twitter.com">
+                <FontAwesomeIcon
+                  icon={faTwitter}
+                  className="w-7 h-7 mt-3 text-teal-500 hover:text-slate-300"
+                />
+              </a>
+            </div>
+            <div>
+              <a href="http://youtube.com">
+                <FontAwesomeIcon
+                  icon={faYoutube}
+                  className="w-7 h-7 mt-3 text-teal-500 hover:text-slate-300"
+                />
+              </a>
+            </div>
+          </Container>
+        </div>
+
+        <div className="ml-10 h-48 w-0.5 mt-8 md:ml-28 bg-white"></div>
+        <div>
+          <div className="ml-7 md:ml-28 mt-10 pb-16">
+            <Link
+              to="/contact"
+              className="text-teal-600 hover:text-slate-300 font-open text-xs text-center mt-10 pb-10"
+            >
+              CONTACT US
+            </Link>
+          </div>
+          <div>
+            <a href="mailto:Info@kauaiteaceremony.com?subject=Mail from Our Site, cc=someoneelse@theirsite.com, another@thatsite.com, me@mysite.com&bcc=lastperson@theirsite.com&subject=Big%20News&body=Body-goes-here">
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                className="w-7 h-7 text-teal-500 hover:text-slate-300 font-open text-sm ml-12 md:ml-32"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
--- Home.js
+++ Home.js
@@ -1,77 +1,110 @@
 import React from "react";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Navbar from "../Components/Navbar";
-// import missionImg from "../Images/1000001568.jpg";
+import missionImg from "../Images/1000001568.jpg";
 import Footer from "../Components/Footer";
-
-import ImgCarousel from "../Components/Footer";
+import ImgCarousel from "../Components/ImgCarousel";
+import ImgCarousel2 from "../Components/ImgCarousel2";
+import MailchimpFormContainer from "../Components/mailChimp";
+import HomePage from "../Images/HomePage.jpg";
 
 const Home = () => {
-  // const navigate = useNavigate();
-  // const { serviceName, productName } = useParams();
+  const navigate = useNavigate();
+
   return (
     <>
       <div>
         <Navbar />
-        <div>
-          <ImgCarousel />
+
+        <ImgCarousel />
+        <ImgCarousel2 />
+      </div>
+      <div>
+        <h1 className="font-open font-thin text-4xl text-center text-teal-400 mt-8">
+          Enjoy Zen moment in a relaxed setting of your choice
+        </h1>
+      </div>
+      <div className=" grid-cols-1 md:flex pt-10 md:mb-10">
+        <img
+          src={missionImg}
+          className=" ml-64 w-1/3 h-96 lg:w-80 md:h-96 md:ml-8"
+          alt=""
+        />
+
+        <div className="">
+          <p className="ml-10 mt-8  lg:ml-10 font-open font-thin lg:mr-10 lg:mt-10 text-base">
+            Love is Art. Art is Love. We can express our love and respect
+            through our body. Tea ceremony is a tool to express these things.
+            Tea ceremony is not just a drink, it has spirit.
+            <span className="pl-12 font-open text-base text-teal-600 font-thin">
+              {" "}
+              -Nana{" "}
+            </span>
+          </p>
+          <p className="ml-10 mt-8 lg:pt-8 lg:ml-10 font-open font-thin lg:mr-10 text-base">
+            The purpose of Kauai Tea Ceremony is to bring zen moments into life.
+            Our tea Master Nana says, "unlike drinking tea at your home, your
+            tea is served to you in a very graceful manner at tea ceremony. We
+            hope, through this process, that people realize they are valued and
+            have a part and purpose in their life, just like how they have a
+            role to 'receive the tea'.
+            <span className="pl-12 font-open text-base text-teal-600 font-thin">
+              {" "}
+              -Ayae{" "}
+            </span>
+          </p>
+          <p className="pt-4 pl-10 font-open font-thin lg:mr-10 text-base lg:mb-20">
+            The bonding rituals such as weddings and business retreats can be
+            difficult to navigate. Tea Ceremony offers a relaxing setting to
+            support a sometimes nerve racking activity. Tea Ceremony is meant to
+            put all particants at ease, and to stay present in the moment. We
+            strive to offer time and space that you will truly feel at peace
+            with yourself in the beautiful nature of Kaua'i. Tea Ceremonies are
+            designed to give a unique experience that will stay with you. In our
+            world, we are increasingly finding it difficult to take time to just
+            breath. Our Ceremonies require all participants to take notice to
+            their environment and the very importance of the moment.
+          </p>
+          <h1 className="ml-8 font-open font-thin text-4xl text-center text-teal-400 mt-8 lg:mr-10">
+            Green Tea has positive health benefits for the body
+          </h1>
+          <p className="pt-4 pl-10 font-open font-thin lg:mr-10 text-base">
+            People have hailed the health benefits of green tea for centuries.
+            Studies suggest that consuming green tea may positively affect skin
+            health, help with weight loss, and reduce the risk of cardiovascular
+            disease, among other benefits. Green tea is made from unoxidized
+            leaves and is one of the least processed types of tea. For this
+            reason, it contains the most antioxidants and beneficial
+            polyphenols.
+          </p>
+          <p className="pt-4 pl-10 font-open font-thin lg:mr-10 text-base">
+            Green tea may have a number of health benefits. For example, it may
+            help weight management, skin inflammation, and type 2 diabetes. Some
+            research has also linked green tea consumption to improved
+            cardiovascular health. Green tea has one of the highest
+            concentrations of antioxidants of any tea. It is naturally low in
+            calories and contains less caffeine than black tea and coffee.
+          </p>
+          <p className="pt-4 pl-10 font-open font-thin lg:mr-10 text-base">
+            Zen moments are times you feel peace, calm, and a state of oneness.
+          </p>
+          <p className="pt-4 pl-10 mb-12 lg:mb-6 font-open font-thin lg:mr-10 text-base">
+            We also offer continuous connection to your zen moments through
+            subscribed membership.
+          </p>
+          <img
+            src={HomePage}
+            className="w-1/3 h-96 ml-64  lg:w-64 lg:h-96 lg:-ml-64 lg:-mt-96"
+            alt=""
+          />
         </div>
-        {/* <div className=" bg-yellow-500 h-2 w-screen"></div>
-        <div className="bg-product1 bg-cover bg-center  bg-no-repeat w-screen h-80">
-          <div className="font-tangerine text-white drop-shadow-2xl text-6xl text-center pt-36">
-            Cups and Plates
-          </div>
-          {productName} */}
-        {/* <button
-            onClick={() => {
-              navigate("/:productName");
-            }}
-            className="bg-red-500 text-white font-sans text-sm h-10 w-32 mt-3 ml-96"
-          >
-            SEE PRODUCTS
-          </button> */}
-        {/* </div>
-        <div className="flex pt-16 mb-10">
-          <img src={missionImg} className=" w-2/5 h-1/2 pl-8" alt="" />
-          <div>
-            <p className="pl-10 font-sans pr-10 text-lg">
-              Love is Art, Art is Love. We can express our love and respect
-              through our body. Tea ceremony is a tool to express these things.
-              Tea ceremony is not just a drink, it has spirit.
-              <span className="pl-12 font-sans text-lg font-semibold">
-                {" "}
-                -Nana{" "}
-              </span> */}
-        {/* </p>
-            <p className="pt-8 pl-10 font-sans pr-10 text-lg">
-              The purpose of Suehiro is to bring zen moments into life. Our tea
-              Master Nana says, "unlike drinking tea at your home, your tea is
-              served to you in a very graceful manner at tea ceremony. We hope,
-              through this process, that people realize they are valued and have
-              a part and purpose in their life, just like how they have a role
-              to 'receive the tea'.
-              <span className="pl-12 font-sans text-lg font-semibold">
-                {" "}
-                -Ayae{" "}
-              </span>
-            </p> */}
-        {/* <p className="pt-4 pl-10 font-sans pr-10 text-lg">
-              We also offer continuous connection to your zen moments through
-              subscribed membership.
-            </p>
-            <p className="pt-4 pl-10 font-sans pr-10 text-lg">
-              Zen moments are times you feel peace, calm, and a state of
-              oneness.
-            </p>
-            <p className="pt-4 pl-10 font-sans pr-10 text-lg">
-              We strive to offer time and space that you will truly feel at
-              peace with yourself in the beautiful nature of Kaua'i.
-            </p> */}
-        {/* </div>
-        </div> */}
-        <div>
-          <Footer />
-        </div>
+      </div>
+
+      <MailchimpFormContainer />
+
+      <div>
+        <div className=" md:w-screen md:-mb-2 md:h-1 bg-slate-100"></div>
+        <Footer />
       </div>
     </>
   );
